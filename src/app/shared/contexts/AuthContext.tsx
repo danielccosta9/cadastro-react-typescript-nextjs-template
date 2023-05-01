1a56755de585fb1c12cb93799cbdd01616a39642
danielccosta9 <danielc.costa9@gmail.com>
2023-05-01 00:23:15 -0300

    refactor: removendo o localStorage

--- src/app/shared/contexts/AuthContext.tsx
+++ src/app/shared/contexts/AuthContext.tsx
@@ -21,14 +21,12 @@ export const AuthProvider: React.FC<AuthProviderProps> = ({ children }) => {
     const [accessToken, setAccessToken] = useState<string>();
 
     useEffect(() => {
-        const accessToken = localStorage.getItem(LOCAL_STOREGE_KEY__ACCESS_TOKEN);
-
         if (accessToken) {
             setAccessToken(accessToken);
         } else {
             setAccessToken(undefined);
         }
-    }, []);
+    }, [accessToken]);
 
     const handleLogin = useCallback(async (email: string, password: string) => {
         const result = await AuthService.auth(email, password);
@@ -36,13 +34,11 @@ export const AuthProvider: React.FC<AuthProviderProps> = ({ children }) => {
         if (result instanceof Error) {
             return result.message;
         } else {
-            localStorage.setItem(LOCAL_STOREGE_KEY__ACCESS_TOKEN, JSON.stringify(result.accessToken));
             setAccessToken(result.accessToken);
         }
     }, []);
 
     const handleLogout = useCallback(() => {
-        localStorage.removeItem(LOCAL_STOREGE_KEY__ACCESS_TOKEN);
         setAccessToken(undefined);
     }, []);
 
